af17c749376579544355309f98adfb591e4343bd
SeongJin Kim <sys.ryan0902@gmail.com>
2026-02-20 11:17:18 +0900

    fix(main): use require for passport to fix nodenext module issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
-import * as passport from 'passport';
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const passport = require('passport');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
